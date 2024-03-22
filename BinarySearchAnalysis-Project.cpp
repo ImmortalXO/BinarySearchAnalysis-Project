7dd7b1a4ecfd8519d499a4f70aac8d67f85f8550
ImmortalXO <nlh2576@email.vccs.edu>
2024-03-22 11:21:55 -0400

    Update main.cpp

--- BinarySearchAnalysis-Project.cpp
+++ BinarySearchAnalysis-Project.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <chrono>
 
 using namespace std;
 
@@ -57,36 +58,35 @@ int main() {
 	int SumIBS = 0;
 	int SumSeqS = 0;
 
-	vector<int> nums;
+	int count = 0;
+	while (count < 10) {
 
-	mt19937 rng(random_device{}());
+		vector<int> nums;
 
-	uniform_int_distribution<int> distribution(1, 100);
+		mt19937 rng(random_device{}());
 
-	for (int i = 0; i < 10; ++i) {
-		nums.push_back(distribution(rng));
+		uniform_int_distribution<int> distribution(1, 100);
+
+		for (int i = 0; i < 10; ++i) {
+			nums.push_back(distribution(rng));
+		}
+
+		sort(nums.begin(), nums.end());
+
+		int target = distribution(rng);
+
+		auto start_time = chrono::high_resolution_clock::now();
+
+		auto end_time = chrono::high_resolution_clock::now();
+
+		auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+
+		int result = recursiveBinarySearch(nums, target, 0, nums.size() - 1);
+
+		cout << "Execution time: " << duration << endl;
+
+		count++;
 	}
-
-	sort(nums.begin(), nums.end());
-
-	for (int num : nums) {
-		cout << num << " ";
-	}
-
-	cout << "\n" << endl;
-
-	int target = distribution(rng);
-	int result = recursiveBinarySearch(nums, target, 0, nums.size() - 1);
-
-	cout << target << endl;
-
-	if (result >= 0) {
-		cout << target << " found at index: " << result << endl;
-	}
-	else {
-		cout << target << " was not found, return value is: " << result << endl;
-	}
-	
 
 	return 0;
 }
